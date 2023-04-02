08fdbbad56a3a93a8d2198732e4a3c49966b0c09
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-04-02 19:06:30 +0900

    success

--- _11720.java
+++ _11720.java
@@ -1,21 +1,16 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-// import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class _11720 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        int num = Integer.parseInt(br.readLine());
-        int[] array = new int[num];
-        String S = br.readLine();
-        // StringTokenizer st = new StringTokenizer(S, "");
+        int num = sc.nextInt();
+        String number = Integer.toString(sc.nextInt());
+        char[] array = number.toCharArray();
         int total = 0;
 
-        for(int i = 0; i<num; i++) {
-            array[i] = Integer.parseInt(S.substring(i,i+1));
-            total+=array[i];
+        for(int i=0; i<num; i++) {
+            total+=array[i]-48;
         }
         System.out.println(total);
     }
