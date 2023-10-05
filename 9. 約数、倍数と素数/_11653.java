248570a9496c973b4535737e3dea3aa1d22f952b
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-10-05 23:47:15 +0900

    2023.10.05 Success

---
+++ 9. 約数、倍数と素数/_11653.java
@@ -0,0 +1,31 @@
+// 문제
+// 정수 N이 주어졌을 때, 소인수분해하는 프로그램을 작성하시오.
+
+// 입력
+// 첫째 줄에 정수 N (1 ≤ N ≤ 10,000,000)이 주어진다.
+
+// 출력
+// N의 소인수분해 결과를 한 줄에 하나씩 오름차순으로 출력한다. N이 1인 경우 아무것도 출력하지 않는다.
+
+import java.util.Scanner;
+
+public class _11653 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        while (true) {
+            if (N == 1) {
+                break;
+            }
+            for (int i = 2; i <= N; i++) {
+                if (N % i == 0) {
+                    System.out.println(i);
+                    N /= i;
+                    break;
+                }
+            }
+        }
+    }
+}
