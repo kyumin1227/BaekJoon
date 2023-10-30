d5e38aa3bb094e7fbf3364b555da21bf884375cc
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-10-30 23:02:29 +0900

    2023.10.30 Comment

--- 13. Alignment/_11650.java
+++ 13. Alignment/_11650.java
@@ -9,12 +9,16 @@ public class _11650 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[][] array = new int[N][2];
+        
+        // 배열에 값 입력
         for (int i = 0; i < array.length; i++) {
             String s = br.readLine();
             StringTokenizer st = new StringTokenizer(s);
             array[i][0] = Integer.parseInt(st.nextToken());
             array[i][1] = Integer.parseInt(st.nextToken());
         }
+
+        // 배열 정리
         Arrays.sort(array, (e1, e2) -> {
             if(e1[0] == e2[0]) {
                 return e1[1] - e2[1];
@@ -22,6 +26,8 @@ public class _11650 {
                 return e1[0] - e2[0];
             }
         });
+
+        // 배열 출력
         for (int[] is : array) {
             System.out.println(is[0] + " " + is[1]);
         }
