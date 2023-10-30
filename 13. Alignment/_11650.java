f210a748a15c0ff61034afdf8b8ef4b378d52a4a
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-10-30 22:45:56 +0900

    2023.10.30 Success

--- 13. Alignment/_11650.java
+++ 13. Alignment/_11650.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class _11650 {
@@ -16,24 +15,15 @@ public class _11650 {
             array[i][0] = Integer.parseInt(st.nextToken());
             array[i][1] = Integer.parseInt(st.nextToken());
         }
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for (int i = N - 1; i > 0; i--) {
-            for (int j = 0; j < i; j++) {
-                if (array[j][0] > array[j + 1][0]) {
-                    int[] num = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = num;
-                } else if (array[j][0] == array[j + 1][0] && array[j][1] > array[j + 1][1]) {
-                    int[] num = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = num;
-                }
+        Arrays.sort(array, (e1, e2) -> {
+            if(e1[0] == e2[0]) {
+                return e1[1] - e2[1];
+            } else {
+                return e1[0] - e2[0];
             }
+        });
+        for (int[] is : array) {
+            System.out.println(is[0] + " " + is[1]);
         }
-        for (int i = 0; i < array.length; i++) {
-            bw.write(array[i][0] + " " + array[i][1] + "\n");
-        }
-        bw.flush();
-        bw.close();
     }
 }
