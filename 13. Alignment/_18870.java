489d8d9605288761e7aaf8aa0af6cac86761d1ce
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-10-31 00:30:13 +0900

    2023.10.31 Success - TimeOver로 인해 HashMap 사용

--- 13. Alignment/_18870.java
+++ 13. Alignment/_18870.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class _18870 {
@@ -21,15 +22,16 @@ public class _18870 {
         int[] sortArray = Arrays.stream(array).distinct().toArray();    // 중복을 제거한 배열
         Arrays.sort(sortArray); // 중복 제거한 배열 정렬
 
-        // 값을 중복을 제거한 배열에서 찾은 후 해당 값의 index를 출력
+        // HashMap에 저장
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < sortArray.length; i++) {
+            map.put(sortArray[i], i);
+        }
+
+        // 값을 HashMap에서 찾은 후 출력
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < sortArray.length; j++) {
-                if (array[i] == sortArray[j]) {
-                    bw.write(j + " ");
-                    break;
-                }
-            }
+            bw.write(map.get(array[i]) + " ");
         }
         bw.flush();
         bw.close();
