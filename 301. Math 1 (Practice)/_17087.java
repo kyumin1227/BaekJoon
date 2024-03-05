ea7dfc6fe57c4f9a7928efbffbe7ce3ab93d9f10
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2024-03-05 18:11:16 +0900

    2024.03.05 Success

--- 301. Math 1 (Practice)/_17087.java
+++ 301. Math 1 (Practice)/_17087.java
@@ -21,7 +21,7 @@ public class _17087 {
         
         Arrays.sort(array);
 
-        int[] difs = new int[n];
+        int[] difs = new int[n];    // 각 위치들의 거리를 나타낸 배열
 
         if (s > array[0]) {
             difs[0] = s - array[0];
@@ -35,11 +35,19 @@ public class _17087 {
 
         Arrays.sort(difs);
         
-        int max = 1;
+        int max;
+        // 값이 하나밖에 없다면 최대값은 자동으로 해당 값
+        if (difs.length > 1) {
+            max = 1;
+        } else {
+            max = difs[0];
+        }
+
         int checkNum = max;
         while (checkNum < difs[0]) {
             checkNum++;
             boolean check = true;
+            // 숫자가 각 위치의 거리에 모두 나누어 떨어지는지 확인
             for (int i = 0; i < difs.length; i++) {
                 if (difs[i] % checkNum != 0) {
                     check = false;
