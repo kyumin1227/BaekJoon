fa18f3018c2b9861c5fed71f64cf8492e63a4565
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2024-03-07 19:17:30 +0900

    DP(Dynamic Programming) Practice

--- 00. Java Practice/DP/fibonacci.java
+++ 00. Java Practice/DP/fibonacci.java
@@ -3,15 +3,20 @@ package DP;
 import java.util.Scanner;
 
 public class fibonacci {
+    static int[] dp;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        dp = new int[n + 1];
         System.out.println(fibo(n));
     }
     
     public static int fibo(int x) {
         if (x == 1 || x == 2)
             return 1;
-        return fibo(x - 1) + fibo(x - 2);
+        if (dp[x] != 0)
+            return dp[x];
+        dp[x] = fibo(x - 1) + fibo(x - 2);
+        return dp[x];
     }
 }
--- 00. Python Practice/DP/fibonacci.py
+++ 00. Python Practice/DP/fibonacci.py
@@ -1,8 +1,13 @@
 def fibo(x):
     if(x == 1 or x == 2):
         return 1
-    return fibo(x - 1) + fibo(x - 2)
+    if(dp[x] != 0):
+        return dp[x]
+    dp[x] = fibo(x - 1) + fibo(x - 2)
+    return dp[x]
 
 n = int(input())
+
+dp = [0] * (n + 1)
 
 print(fibo(n))
