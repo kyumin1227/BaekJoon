259d3f9ce7c04be3ed7db562ded18ce8e6afcff7
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2024-03-10 22:26:09 +0900

    2024.03.10 Success

--- 400. Dynamic Programming 1/_9095.java
+++ 400. Dynamic Programming 1/_9095.java
@@ -3,29 +3,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class _9095 {
-    static int count;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int t = Integer.parseInt(br.readLine());
 
+        int[] dp = new int[12];
+
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
         for (int i = 0; i < t; i++) {
-            cal(Integer.parseInt(br.readLine()));
+            int n = Integer.parseInt(br.readLine());
 
-            System.out.println(count);
-        }
-    }
-    
-    public static void cal(int x) {
-        if (x == 0) {
-            count++;
-        } else if (x < 0) {
-
-        } else {
-            cal(x - 1);
-            cal(x - 2);
-            cal(x - 3);
+            if (n > 3) {
+                for (int j = 4; j <= n; j++) {
+                    dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3];
+                }
+                System.out.println(dp[n]);
+            } else {
+                System.out.println(dp[n]);
+            }
         }
     }
 }
