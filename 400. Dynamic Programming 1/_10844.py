0789c7020ea3ecadea06826bdd8ffc5d00b88fff
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2024-03-12 23:45:23 +0900

    2024.03.12 Success

--- 400. Dynamic Programming 1/_10844.py
+++ 400. Dynamic Programming 1/_10844.py
@@ -1,23 +1,20 @@
-def cal(x, i):
-    if (i == 0):
-        bp[n - 1] += 1
-        return
-    if (x == 0):
-        cal(x + 1, i - 1)
-        return   
-    if (x == 9):
-        cal(x - 1, i - 1)
-        return
-    cal(x + 1, i - 1)
-    cal(x - 1, i - 1)
-    return
-
-
 n = int(input())
 
-bp = [0] * n
+dp = [[0 for _ in range(10)] for _ in range(n + 1)]
 
-for i in range(1 , 10):
-    cal(i, n - 1)
+for i in range(1, 10):
+    dp[1][i] = 1
+
+for i in range(2, n + 1):
+    dp[i][0] = dp[i - 1][1]
+    dp[i][9] = dp[i - 1][8]
+    
+    for j in range(1, 9):
+        dp[i][j] = dp[i - 1][j + 1] + dp[i - 1][j - 1]
         
-print(bp[n - 1] % 1000000000)
+result = 0
+
+for i in range(0, 10):
+    result += dp[n][i]
+        
+print(result % 1000000000)
