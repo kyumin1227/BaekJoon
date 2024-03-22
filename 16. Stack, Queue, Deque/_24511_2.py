693c6f0f052883c6c39489302701555c3368a994
kyumin1227 <kyumin12271227@gmail.com>
2024-03-22 18:08:37 +0900

    2024.03.22 Success

--- 16. Stack, Queue, Deque/_24511_2.py
+++ 16. Stack, Queue, Deque/_24511_2.py
@@ -1,5 +1,8 @@
 # 값을 입력 받고 queue인 값만 deque에 저장
 import collections
+import sys
+
+input = sys.stdin.readline
 
 n = int(input())    # 자료구조의 개수
 list_stack_or_queue = list(input().split()) # 큐인지 스택인지 구분하는 리스트
@@ -8,7 +11,6 @@ m = int(input())    # 삽입할 수열의 길이
 input_value = list(map(int, input().split()))   # 삽입할 값
 
 main_deque = collections.deque()
-result = ""
 
 for index, value in enumerate(list_stack_or_queue):
     if value == "1":
@@ -18,7 +20,4 @@ for index, value in enumerate(list_stack_or_queue):
 # 입력 값을 deque에 넣고 가장 끝 값을 뺌
 for value in input_value:
     main_deque.appendleft(value)
-    result += str(main_deque.pop()) + " "
-
-# 가장 끝 값만 모아서 정답으로 출력
-print(result)
+    print(main_deque.pop(), end=" ")
