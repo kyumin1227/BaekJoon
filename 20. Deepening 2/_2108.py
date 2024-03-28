0fa26c5d1b3fe0eb5a70809c335eb57783b92d65
kyumin1227 <kyumin12271227@gmail.com>
2024-03-28 23:33:59 +0900

    2024.03.28 Success

--- 20. Deepening 2/_2108.py
+++ 20. Deepening 2/_2108.py
@@ -5,28 +5,34 @@ input = sys.stdin.readline
 n = int(input())
 
 num_list = []
-num_set = set()
+num_dic = dict()
 sum = 0
 
 for _ in range(n):
     num = int(input())
 
-    num_set.add(num)
     num_list.append(num)
     sum += num
+    if num in num_dic:
+        num_dic[num] += 1
+    else:
+        num_dic[num] = 1
 
 num_list.sort()
 
-mode_count = 0
+mode_count = max(num_dic.values())
+mode_nums = []
 mode_num = 0
 
-# 최빈값 구하기
-for i in num_set:
-    i_count = num_list.count(i)
+for key, value in num_dic.items():
+    if value == mode_count:
+        mode_nums.append(key)
 
-    if mode_count < i_count:
-        mode_num = i
-        mode_count = i_count
+if len(mode_nums) > 1:
+    mode_nums.sort()
+    mode_num = mode_nums[1]
+else:
+    mode_num = mode_nums[0]
 
 print(int(round(sum / n, 0)))           # 평균
 print(num_list[n // 2])                 # 중앙값
