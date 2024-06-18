68f961f7f61dfc9950858dd7c5b0ebececb01123
kyumin1227 <kyumin12271227@gmail.com>
2024-06-18 13:31:34 +0900

    2024.06.18 Success

---
+++ 22. Backtracking/_9663.py
@@ -0,0 +1,49 @@
+def changeBoard(arg_x, arg_y, arg_z, arg_bool):
+    target_list[arg_x][arg_y][arg_z] = arg_bool
+
+    for i in range(n):
+        target_list[i][arg_y][arg_z] = arg_bool
+    
+    i = 1
+    while arg_x + i < n and arg_y + i < n:
+        target_list[arg_x + i][arg_y + i][arg_z] = arg_bool
+        i += 1
+
+    i = 1
+    while arg_x + i < n and arg_y - i >= 0:
+        target_list[arg_x + i][arg_y - i][arg_z] = arg_bool
+        i += 1
+
+
+def backtracking(arg_x, arg_y, arg_z):
+    changeBoard(arg_x, arg_y, arg_z, True)
+    global result
+    index = len(queen_list)
+    if index == n:
+        result += 1
+        return
+
+    for j in range(n):
+        isEmpty = True
+        for target in target_list[index][j]:
+            if target != False:
+                isEmpty = False
+                break
+        if isEmpty:
+            queen_list.append((index, j))
+            backtracking(index, j, arg_z + 1)
+            changeBoard(*queen_list.pop(), len(queen_list), False)
+
+
+n = int(input())
+
+target_list = [[[False for _ in range(n)] for _ in range(n)] for _ in range(n)]
+queen_list = []
+result = 0
+
+for i in range(n):
+    queen_list.append((0, i))
+    backtracking(0, i, 0)
+    changeBoard(*queen_list.pop(), 0, False)
+
+print(result)
