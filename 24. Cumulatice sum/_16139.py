2243d5544f13bbb811fa86b82c1a38606815cd53
kyumin1227 <kyumin12271227@gmail.com>
2024-07-05 19:47:14 +0900

    2024.07.05 Success(50)

--- 24. Cumulatice sum/_16139.py
+++ 24. Cumulatice sum/_16139.py
@@ -4,6 +4,15 @@ s = stdin.readline().rstrip()
 
 n = int(stdin.readline().rstrip())
 
+dp = [[0 for _ in range(26)] for _ in range(len(s))]
+
+dp[0][ord(s[0]) - 97] += 1
+
+for i in range(1, len(s)):
+    for j in range(26):
+        dp[i][j] = dp[i - 1][j]
+    dp[i][ord(s[i]) - 97] += 1
+
 t_list = stdin.readlines()
 
 for t in t_list:
@@ -12,10 +21,7 @@ for t in t_list:
     start_index = int(start_index)
     end_index = int(end_index)
 
-    count = 0
-
-    for i in range(start_index, end_index + 1):
-        if s[i] == c:
-            count += 1
-
-    print(count)
+    if start_index == 0:
+        print(dp[end_index][ord(c) - 97])
+    else:
+        print(dp[end_index][ord(c) - 97] - dp[start_index - 1][ord(c) - 97])
