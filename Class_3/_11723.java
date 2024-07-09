a23216ee44a763712fcb4d4f4bef9a35ac2f6f65
kyumin1227 <kyumin12271227@gmail.com>
2024-07-09 13:37:34 +0900

    2024.07.09 Success

--- Class_3/_11723.java
+++ Class_3/_11723.java
@@ -1,12 +1,15 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class _11723 {
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         boolean[] targetArray = new boolean[20];
         int n = Integer.parseInt(br.readLine());
@@ -22,9 +25,9 @@ public class _11723 {
                 targetArray[Integer.parseInt(st.nextToken()) - 1] = false;
             } else if (s.equals("check")) {
                 if (targetArray[Integer.parseInt(st.nextToken()) - 1]) {
-                    System.out.println(1);
+                    bw.write("1\n");
                 } else {
-                    System.out.println(0);
+                    bw.write("0\n");
                 }
             } else if (s.equals("toggle")) {
                 int index = Integer.parseInt(st.nextToken()) - 1;
@@ -39,5 +42,9 @@ public class _11723 {
                 }
             }
         }
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
