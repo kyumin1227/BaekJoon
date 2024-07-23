d3955c6d0be88d3f80b2de8b78d0387d43d9a3ec
kyumin1227 <kyumin12271227@gmail.com>
2024-07-23 20:38:51 +0900

    Success

---
+++ 21. Recursion/_25501.py
@@ -0,0 +1,17 @@
+import sys
+
+def recursion(s, l, r, count):
+    count += 1
+    if l >= r: return 1, count
+    elif s[l] != s[r]: return 0, count
+    else: return recursion(s, l+1, r-1, count)
+
+def isPalindrome(s):
+    return recursion(s, 0, len(s)-1, 0)
+
+input = sys.stdin.readline
+
+n = int(input())
+
+for i in range(n):
+    print(*isPalindrome(input().rstrip()))
